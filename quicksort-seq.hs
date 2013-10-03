2e82de1153eaea56a778ded9dc22ebeef9a4d8e3
alvare <welcometothechango@gmail.com>
2013-10-02 23:45:19 -0300

    quicksort with Data.Seq

--- quicksort-seq.hs
+++ quicksort-seq.hs
@@ -1,8 +1,10 @@
 import qualified Data.Sequence as Seq
--- 1
+import Data.Sequence ((><), (<|), (|>))
+
 quicksort :: Seq.Seq Int -> Seq.Seq Int
-quicksort empty = empty
-quicksort (singleton x) = singleton x
---quicksort (x:xs) = (quicksort menores) ++ [x] ++ (quicksort mayores)
---    where menores = filter (<x) xs
---          mayores = filter (>=x) xs
+quicksort xxs | Seq.null xxs = Seq.empty
+quicksort xxs = (quicksort menores) >< (Seq.singleton x) >< (quicksort mayores)
+    where menores = Seq.filter (<x) cola
+          mayores = Seq.filter (>=x) cola
+          x = Seq.index xxs 0 
+          cola = Seq.drop 1 xxs
